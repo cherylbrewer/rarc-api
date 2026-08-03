396b1d3a4d4ce06348929271301c68cdd585ff4d
cherylbrewer <brewerdatasolutions@gmail.com>
2026-08-02 21:15:08 -0500

    Update carcMetadata.js

--- data/carcMetadata.js
+++ data/carcMetadata.js
@@ -2,5 +2,6 @@ export default {
   version: "1.0.0",
   releaseDate: "2026-08-02",
   source: "Brewer Data Solutions",
-  description: "CARC Denial Intelligence Crosswalk"
+  description: "CARC Denial Intelligence Crosswalk",
+  x12Version: "Current"
 };
